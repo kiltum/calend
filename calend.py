91702e7b9a2531ca6f0c4d243dfd31d8b9405cfd
Viacheslav Kaloshin <kiltum@kiltum.tech>
2017-10-26 10:31:50 +0300

    pep8 changes

--- calend.py
+++ calend.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # sudo pip install feedparser
+# pep8 --ignore=W191,E501 calend.py
 
 import feedparser
 import time
 import locale
 import sys
+
 
 def get_today():
 	f = feedparser.parse('http://www.calend.ru/img/export/today-holidays.rss')
@@ -16,8 +18,9 @@ def get_today():
 	hol = now + ": "
 	for i in f.entries:
 		if now in i['title']:
-			hol = hol + i['title'].split('-')[1].strip()+ "; "
+			hol = hol + i['title'].split('-')[1].strip() + "; "
 	return hol[:-2]  # remove trailing ;
+
 
 def main():
 	print get_today()
