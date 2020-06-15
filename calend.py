7683323e92fd118fd78d829635727e8d8d64dfa9
Viacheslav Kaloshin <kiltum@kiltum.tech>
2020-06-15 14:18:25 +0300

    moving to python3

--- calend.py
+++ calend.py
@@ -1,29 +1,27 @@
-#!/usr/bin/env python
-# sudo pip install feedparser
-# pep8 --ignore=W191,E501 calend.py
+#!/usr/bin/env python3
+# sudo pip3 install feedparser
+
+import locale
+import time
 
 import feedparser
-import time
-import locale
-import sys
 
 
 def get_today():
-	f = feedparser.parse('http://www.calend.ru/img/export/today-holidays.rss')
-	locale.setlocale(locale.LC_TIME, "ru_RU")
-	reload(sys)
-	sys.setdefaultencoding('utf8')
-	now = "%s %s" % (int(time.strftime("%d")), time.strftime("%B"))
+    f = feedparser.parse('http://www.calend.ru/img/export/today-holidays.rss')
+    locale.setlocale(locale.LC_TIME, "ru_RU")
+    now = "%s %s" % (int(time.strftime("%d")), time.strftime("%B"))
 
-	hol = now + ": "
-	for i in f.entries:
-		if now in i['title']:
-			hol = hol + i['title'].split('-')[1].strip() + "; "
-	return hol[:-2]  # remove trailing ;
+    hol = now + ": "
+    for i in f.entries:
+        if now in i['title']:
+            hol = hol + i['title'].split('-')[1].strip() + "; "
+    return hol[:-2]  # remove trailing ;
 
 
 def main():
-	print get_today()
+    print(get_today())
+
 
 if __name__ == "__main__":
-	main()
+    main()
